2c552231ec5ee26a49ebbb975ee178d60de3b741
Erison88 <songerison.mse08@nctu.edu.tw>
2023-02-17 23:28:30 +0800

    add insertion sort algorithm

--- hw1/insertion_sort.cpp
+++ hw1/insertion_sort.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main(){
-    cout << "this is a test!" << endl;
+    int a[] = {10,3,2,6,1,8,7};
+    int key = 0;
+    for(int j=0; j<7; j++){
+        key = a[j];
+        //insert a[j] into the sorted a[1,2,...]
+        int i = j-1;
+        while(i>=0 && a[i]>key){
+            a[i+1] = a[i];
+            i = i-1;
+        }
+        a[i+1] = key;
+    }
+
+    //print a[]
+    for(int i=0; i<7; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    cout << "this is the last line!" << endl;
+
     return 0;
 }
