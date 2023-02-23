024e69eabb0b7b45e8b2a32ab51cd2f5fa1b9bb6
Erison88 <songerison.mse08@nctu.edu.tw>
2023-02-23 13:51:32 +0800

    add divide_and_conquer function

--- hw2/divide_and_conquer.cpp
+++ hw2/divide_and_conquer.cpp
@@ -5,6 +5,7 @@ using namespace std;
 //forward delcaration
 int brutal(int *arr, int size);
 int GetCrossMax(int *arr, int size, int start, int middle, int end);
+int divide_and_conquer(int *arr, int size, int start, int end);
 
 int main(){
     cout << "this is the last line!" << endl;
@@ -31,7 +32,25 @@ int GetCrossMax(int *arr, int size, int start, int middle, int end){
     int leftMax = arr[middle], leftSum = 0;
     int rightMax = arr[middle + 1], rightSum =0;
 
-    for(int i = middle+1; i>=start; i--){
-        //test
+    for(int i = middle; i>=start; i--){
+        leftSum += arr[i];
+        leftMax = max(leftMax, leftSum);
     }
+    for(int i=middle+1; i<=end; i++){
+        rightSum += arr[i];
+        rightMax = max(rightMax, rightSum);
+    }
+    return max(max(leftMax, rightMax), leftMax+rightMax);
+}
+
+int divide_and_conquer(int *arr, int size, int start, int end){
+    if(start == end)
+        return arr[start];
+    
+    int middle = (start+end)/2;
+    int leftMax = divide_and_conquer(arr, size, start, middle);
+    int rightMax = divide_and_conquer(arr,size, middle+1, end);
+    int cross = GetCrossMax(arr, size, start, middle, end);
+
+    return max(max(leftMax, rightMax), cross);
 }
