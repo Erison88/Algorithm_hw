7f089f33fdfe9db0ea65da5bed16b4fa633e3716
Erison88 <songerison.mse08@nctu.edu.tw>
2023-03-06 23:18:40 +0800

    data become float and finish read file

--- hw2/FindMaxArray.cpp
+++ hw2/FindMaxArray.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 //forward delcaration
 int brutal(int *arr, int size);
-int FindCrossMax(int *arr, int start, int middle, int end);
-int FindMaxArray(int *arr, int start, int end);
+float FindCrossMax(float *arr, int start, int middle, int end);
+float FindMaxArray(float *arr, int start, int end);
 
 int main(int argc, char ** argv){
     //input file
@@ -30,22 +30,21 @@ int main(int argc, char ** argv){
         cout << "Input file is filed." << endl;
     }
 
-    int *data = new int[datanum];
+    float *data = new float[datanum];
     int index;  // to store the index number
     while(inputstream >> index){
         cout << index << " ";
-        inputstream >> data[0];
+        inputstream >> data[index-1];
     }
     cout << endl;
-    cout << data[0] << endl;
     
     inputstream.close();
-    /*
+    
     for(int i=0; i<datanum; i++){
         cout << data[i] <<" ";
     }
     cout << endl;
-    */
+    
     cout << datanum << endl;
     delete [] data;
 
@@ -69,11 +68,11 @@ int brutal(int *arr, int size){
     return max;
 }
 
-int FindCrossMax(int *arr, int start, int middle, int end){
-    int leftMax = arr[middle];
-    int rightMax = arr[middle + 1];
-    int leftSum = 0;
-    int rightSum = 0;
+float FindCrossMax(float *arr, int start, int middle, int end){
+    float leftMax = arr[middle];
+    float rightMax = arr[middle + 1];
+    float leftSum = 0.0;
+    float rightSum = 0.0;
 
     for(int i = middle; i>=start; i--){
         leftSum += arr[i];
@@ -86,14 +85,14 @@ int FindCrossMax(int *arr, int start, int middle, int end){
     return max(max(leftMax, rightMax), leftMax+rightMax);   // find the Max from the three subsection 
 }
 
-int FindMaxArray(int *arr, int start, int end){
+float FindMaxArray(float *arr, int start, int end){
     if(start == end)            // base case: only one element
         return arr[start];
     
     int middle = (start+end)/2;
-    int leftMax = FindMaxArray(arr, start, middle);      // find left Max
-    int rightMax = FindMaxArray(arr, middle+1, end);     // find right Max
-    int cross = FindCrossMax(arr, start, middle, end);   // find the Max from the three section 
+    float leftMax = FindMaxArray(arr, start, middle);      // find left Max
+    float rightMax = FindMaxArray(arr, middle+1, end);     // find right Max
+    float cross = FindCrossMax(arr, start, middle, end);   // find the Max from the three section 
 
     return max(max(leftMax, rightMax), cross);
 }
